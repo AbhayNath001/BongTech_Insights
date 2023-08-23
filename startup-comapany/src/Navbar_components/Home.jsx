9da3e6da64ccc5840d4cc4a8e17e1076ea174d1a
Abhay Nath <103759014+AbhayNath001@users.noreply.github.com>
2023-08-23 14:11:13 +0530

    Add files via upload

---
+++ startup-comapany/src/Navbar_components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="nav">
+      Home Page
+    </div>
+  );
+}
+export default Home;
---
+++ startup-comapany/src/Navbar_components/Verification.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Verification() {
+  return (
+    <div className="nav">
+      Verification Page
+    </div>
+  );
+}
+export default Verification;
