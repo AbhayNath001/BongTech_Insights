4e5f0ea6afc1f68042def4423cd51e6fae19e85a
Abhay Nath <103759014+AbhayNath001@users.noreply.github.com>
2023-08-25 15:59:48 +0530

    Add files via upload
    work on home page

--- startup-comapany/src/Navbar_components/Home.jsx
+++ startup-comapany/src/Navbar_components/Home.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 function Home() {
   return (
-    <div className="nav">
-      Home Page
+    <div className="home">
+      Welcome to BongTech Insights
+      <div className="big_header">
+        Leading Software <br/><span>Technology</span><br/> Company
+      </div>
     </div>
   );
 }
