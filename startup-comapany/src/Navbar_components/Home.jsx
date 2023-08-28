802b2673941d7e832aa08be63d18be2c9b962b4e
Abhay Nath <103759014+AbhayNath001@users.noreply.github.com>
2023-08-28 15:50:57 +0530

    Add files via upload
    update team section

--- startup-comapany/src/Navbar_components/Home.jsx
+++ startup-comapany/src/Navbar_components/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const navigate = useNavigate();
+
+  const handleApplyClick = () => {
+    navigate('/internship');
+  };
+
   return (
     <div className="home">
-      Welcome to BongTech Insights
+      <b>Welcome to BongTech Insights</b>
       <div className="big_header">
-        Leading Software <br/><span>Technology</span><br/> Company
+        Leading Software <br/><span>Technology</span><br/> Company<br/>
+      </div>
+      <div class="apply_button">
+      <button type="submit" onClick={handleApplyClick}>Apply Here →</button>
       </div>
     </div>
   );
--- startup-comapany/src/Navbar_components/Team.jsx
+++ startup-comapany/src/Navbar_components/Team.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import Abhay_Nath from './Team_images/Abhay_Nath.jpg';
 
 function Contact() {
   return (
-    <div className="nav">
-      Team Page
+    <div>
+      <div className="team">
+        <div className="team_heading">
+          <div className="team_item">
+            <h1>Team</h1>
+            <h3>🏠 Home → Team</h3>
+          </div>
+        </div>
+      </div>
+      <div className="professional_people">
+        <div>
+          <h3>Professional People</h3>
+          <h1>Meet our Team members</h1>
+        </div>
+        <div class="vertical-line"></div>
+        <div>
+        <h2>Our dedicated team actively provides continous support and task accomplishments in businesses.</h2>
+        </div>
+      </div>
+      <div className="team_img">
+        <div className="tm_img">
+          <img src={Abhay_Nath} alt="Abhay Nath" />
+          <h2><span>Abhay</span> Nath</h2>
+          <p>CEO & Founder</p>
+        </div>
+      </div>
     </div>
   );
 }
